5516d332ed5f14a196ae362c1439cc02494ba9af
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-11-02 01:52:17 +0200

    🔧 Update 'push.ps1' to fail when not provide NUGET_API_URL or NUGET_API_KEY - skip-ci'

--- scripts/push.ps1
+++ scripts/push.ps1
@@ -3,21 +3,21 @@
 $scriptName = $MyInvocation.MyCommand.Name
 
 if ([string]::IsNullOrEmpty($env:NUGET_API_URL)) {
-  Write-Host "${scriptName}: NUGET_API_URL is empty or not set. Skipped pushing package(s)."
+  throw "${scriptName}: NUGET_API_URL is empty or not set. Skipped pushing package(s)."
 }
-elseif ([string]::IsNullOrEmpty($env:NUGET_API_KEY)) {
-  Write-Host "${scriptName}: NUGET_API_KEY is empty or not set. Skipped pushing package(s)."
+
+if ([string]::IsNullOrEmpty($env:NUGET_API_KEY)) {
+  throw "${scriptName}: NUGET_API_KEY is empty or not set. Skipped pushing package(s)."
 }
-else {
-  Push-Location
 
-  $path = Join-Path $rootPath $artifactsDir
-  Set-Location $path
+Push-Location
 
-  Get-ChildItem $path -Filter "*.nupkg" | ForEach-Object {
-    Write-Host "$($scriptName): Pushing $($_.Name)"
-    exec { & dotnet nuget push $_ --source $env:NUGET_API_URL --api-key $env:NUGET_API_KEY }
-  }
+$path = Join-Path $rootPath $artifactsDir
+Set-Location $path
 
-  Pop-Location
+Get-ChildItem $path -Filter "*.nupkg" | ForEach-Object {
+  Write-Host "$($scriptName): Pushing $($_.Name)"
+  exec { & dotnet nuget push $_ --source $env:NUGET_API_URL --api-key $env:NUGET_API_KEY }
 }
+
+Pop-Location
