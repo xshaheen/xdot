53eee25159c43162075144246428a50b5f8dd7bc
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-11-18 19:52:08 +0200

    Minor edit in build script

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -2,11 +2,20 @@
 
 if (Test-Path $artifactsDir) { Remove-Item $artifactsDir -Force -Recurse }
 
-Write-Host "### Clean Sln..."
+Write-Host ""
+Write-Host "#####################"
+Write-Host "###   Clean Sln   ###"
+Write-Host "#####################"
 exec { & dotnet clean -c Release }
 
-Write-Host "### Build Sln..."
+Write-Host ""
+Write-Host "#####################"
+Write-Host "###   Build Sln   ###"
+Write-Host "#####################"
 exec { & dotnet build -c Release }
 
-Write-Host "### Pack..."
+Write-Host ""
+Write-Host "#####################"
+Write-Host "###      Pack     ###"
+Write-Host "#####################"
 exec { & dotnet pack -c Release -o $artifactsDir --no-build }
