4be98ff9c7d8f59c9543433180bbbc10652ec0e4
Mahmoud Shaheen <mxshaheen@outlook.com>
2021-01-02 21:28:42 +0200

    Enhance UserBase

--- src/Domain.Identity/IdentityUserBase.cs
+++ src/Domain.Identity/IdentityUserBase.cs
@@ -4,42 +4,49 @@ using System.Linq;
 using Microsoft.AspNetCore.Identity;
 
 namespace X.Domain.Identity {
-    public abstract class
-        IdentityUserBase : IdentityUser, IEquatable<IdentityUserBase>, IAuditable {
+    public abstract class IdentityUserBase<TModifier>
+        : IdentityUser, IEquatable<IdentityUserBase<TModifier>>, IAuditable<TModifier> {
         public DateTime LastSeen { get; set; }
         public DateTime CreatedAt { get; protected set; }
-        public string? CreatedBy { get; protected set; }
-        public DateTime? LastModifiedAt { get; private set; }
-        public string? LastModifiedBy { get; private set; }
+        public DateTime? LastModifiedAt { get; protected set; }
+        public string? LastModifiedById { get; protected set; }
+        public TModifier? LastModifiedBy { get; protected init; }
 
-        public void SetCreatedAtBy(DateTime at, string by) => (CreatedAt, CreatedBy) = (at, by);
+        public void SetLasModification(DateTime at, string by)
+            => (LastModifiedAt, LastModifiedById) = (at, by);
 
-        public void UpdateLasModifiedAtBy(DateTime at, string by)
-            => (LastModifiedAt, LastModifiedBy) = (at, by);
+        public bool Equals(IdentityUserBase<TModifier>? other) {
+            if (other is null)
+                return false;
 
-        public bool Equals(IdentityUserBase? other) {
-            if (other is null) return false;
+            if (ReferenceEquals(this, other))
+                return true;
 
-            if (ReferenceEquals(this, other)) return true;
-
-            if (GetType() != other.GetType()) return false;
+            if (GetType() != other.GetType())
+                return false;
 
             return Equals().SequenceEqual(other.Equals());
         }
 
-        public static bool operator==(IdentityUserBase? left, IdentityUserBase? right) {
-            if (left is null ^ right is null) return false;
+        public static bool operator==(
+            IdentityUserBase<TModifier>? left,
+            IdentityUserBase<TModifier>? right
+        ) {
+            if (left is null ^ right is null)
+                return false;
             return left?.Equals(right) != false;
         }
 
-        public static bool operator!=(IdentityUserBase left, IdentityUserBase right)
-            => !(left == right);
+        public static bool operator!=(
+            IdentityUserBase<TModifier> left,
+            IdentityUserBase<TModifier> right
+        ) => !(left == right);
 
-        public sealed override bool Equals(object? obj) => Equals(obj as IdentityUserBase);
+        public sealed override bool Equals(object? obj)
+            => Equals(obj as IdentityUserBase<TModifier>);
 
-        public override int GetHashCode()
-            => Equals().Select(x => x != null ? x.GetHashCode() : 0)
-                .Aggregate((x, y) => x ^ y);
+        public override int GetHashCode() => Equals().Select(x => x != null ? x.GetHashCode() : 0)
+            .Aggregate((x, y) => x ^ y);
 
         private IEnumerable<object?> Equals() { yield return Id; }
     }
